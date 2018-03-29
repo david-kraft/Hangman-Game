169307a0926baa6552b651af5eccb5597a095a43
David Kraft <kraft153@umn.edu>
2018-03-29 00:13:49 -0500

    Made some comments and plans for next steps. Tomorrow I will plan the 'choice()' function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -18,16 +18,20 @@ function startGame(){
         // Displays the letter bank
         for (i = 0; i < alphabetString.length; i++) {
             document.getElementById("letterBank").innerHTML += '<button onclick="choice("' + alphabetArray[i] + '")" class="btn btn-light btn-lg rounded-circle m-1" id="' + alphabetArray[i] + '-button"><span class="bank-letter">' + alphabetArray[i] + '</span>'
-
         }
-    // 
 
-    // Generate Random Word
-    var puzzleWord = wordBank[Math.floor(Math.Random * wordBank.length)];
-    console.log ("puzzleWord = " + puzzleWord);
-    // Split puzzleWord into an array with one letter per item
-    var puzzleArray =puzzleWord.split("");
-    console.log ("puzzleArray = " + puzzleArray);
+    // Generate Word
+
+        // Select random string item from array
+        var puzzleWord = wordBank[Math.floor(Math.Random * wordBank.length)];
+        console.log ("puzzleWord = " + puzzleWord);
+
+        // Split puzzleWord into an array with one letter per item
+        var puzzleArray =puzzleWord.split("");
+        console.log ("puzzleArray = " + puzzleArray);
+
+    // Generate alert divs through a for-loop that contain fixed-width spaces corresponding to the items in the puzzleArray. These alert divs should have ids that match the position in the word or the value of "i" in the for-loop and should have classes to represent the letters. Log the classes, ids of the divs into the console.
+
 
     
 
