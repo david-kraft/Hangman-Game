8f01941a2bb46be09cfc61c6b0b2fa8e794e4500
David Kraft <kraft153@umn.edu>
2018-05-02 16:59:45 -0500

    Almost complete.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,46 +1,72 @@
 // wordBank contains items that would be cute or comforting to a baby.
 var wordBank = ["BUNNY", "KITTEN", "SQUIRREL", "BLANKET", "PILLOW", "MOBILE", "SWING", "CLOWN"];
 
-document.getElementById("babyPicture").onclick = function() {startGame()};
 
-function startGame(){
+// Split alphabet into an array with one letter per item
+var alphabetArray = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+console.log("alphabetArray = " + alphabetArray);
 
-    // Display the letter bank
 
-        // assign the upper-case alphabet to a string variable
-        var alphabetString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        console.log ("alphabetString = " + alphabetString);
+// The triggering event is in the HTML. That way I can disable it during the game and re-enable it for game over
+function startGame() {
 
-        // Split alphabet into an array with one letter per item
-        var alphabetArray = alphabetString.split("");
-        console.log ("alphabetArray = " + alphabetArray);
+    // disables the button that starts the game
+    document.getElementById("babyPicture").onclick = null;
 
-        // Displays the letter bank
-        for (i = 0; i < alphabetString.length; i++) {
-            document.getElementById("letterBank").innerHTML += '<button onclick="choice("' + alphabetArray[i] + '")" class="btn btn-light btn-lg rounded-circle m-1" id="' + alphabetArray[i] + '-button"><span class="bank-letter">' + alphabetArray[i] + '</span>'
-        }
+
+    // Displays the letter bank
+    for (i = 0; i < alphabetArray.length; i++) {
+        document.getElementById("letterBank").innerHTML += '<button onclick="choice("' + alphabetArray[i] + '")" class="btn btn-light btn-lg rounded-circle m-1" id="' + alphabetArray[i] + '-button"><span class="bank-letter">' + alphabetArray[i] + '</span>'
+    }
 
     // Generate Word
 
-        // Select random string item from array
-        var puzzleWord = wordBank[Math.floor(Math.Random * wordBank.length)];
-        console.log ("puzzleWord = " + puzzleWord);
+    // Select random string item from array
+    var puzzleWord = wordBank[Math.floor(Math.random() * wordBank.length)];
+    console.log("puzzleWord: " + puzzleWord);
+    var puzzleArray = puzzleWord.split("")
+    console.log("puzzleArray: " + puzzleArray)
 
-        // Split puzzleWord into an array with one letter per item
-        var puzzleArray =puzzleWord.split("");
-        console.log ("puzzleArray = " + puzzleArray);
+    var blank = "_";
+    var blankWord = "";
+    var l = puzzleWord.length;
+    blankWord = blank.repeat(l);
+    console.log("blankWord: " + blankWord);
 
-    // Generate alert divs through a for-loop that contain fixed-width spaces corresponding to the items in the puzzleArray. These alert divs should have ids that match the position in the word or the value of "i" in the for-loop and should have classes to represent the letters. Log the classes, ids of the divs into the console.
+    // blankArray will be the split blankWord
+    var blankArray = blankWord.split("");
+    console.log("blankArray: " + blankArray);
+
+    function loadFillingPuzzle() {
+
+        while (getElementById("currentWord").firstChild) getElementById("currentWord").removeChild(getElementById("currentWord").firstChild);
 
 
-    
+        for (i = 0; i < blankArray.length; i++) {
+            document.getElementById("currentWord").innerHTML += '<button class="btn btn-outline-info btn-lg m-1" disabled><span class="bank-letter">' + blankArray[i] + '</span>'
+        }
+    }
 
+    loadFillingPuzzle()
+
+    function choice(letterClicked) {
+        console.log("letterClicked: " + letterClicked);
+
+        // If the letter is not in the word, turns the background of button to red/bootstrap danger
+        if (puzzleWord.indexOf(letterClicked) === -1) {
+            getElementById(letterClicked + "-button").className = "btn btn-danger btn-lg rounded-circle m-1";
+
+
+        } else {
+            getElementById(letterClicked + "-button").className = "btn btn-success btn-lg rounded-circle m-1";
+
+            for (i = 0; i < blankArray.length; i++) {
+                if (puzzleArray[i] === letterClicked) {
+                    blankArray[i] = letterClicked
+                }
+
+            }
+        }
+        loadFillingPuzzle()
+    }
 } /* End of startGame function */
-
-// console.log(puzzleWord)
-
-function choice(){
-    console.log("Clicked")
-    var userGuess = element.getAttribute(attributeName);
-}
-document.addEventListener("click", choice());
